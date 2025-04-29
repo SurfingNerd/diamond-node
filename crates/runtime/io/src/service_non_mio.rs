cb3fa987a8cb55a23fdc1effc2ebae80a95eac11
Thomas Haller <thomashaller@gmx.at>
2025-04-29 14:12:59 +0200

    fixed IoError and IoHandler imports for Non-Mio

--- crates/runtime/io/src/service_non_mio.rs
+++ crates/runtime/io/src/service_non_mio.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with OpenEthereum.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{ioError, ioHandler};
+use crate::{IoError, IoHandler};
 use deque;
 use fnv::FnvHashMap;
 use num_cpus;
