3c5f0083e7c469424286905c8c045295b267d0a8
Thomas Haller <thomashaller@gmx.at>
2026-01-07 16:56:58 +0100

    comments for proposing the queued_transactions(). This needs further investigation about the theory, that the empty blocks are the result of a Nodes proposing transactions that were already imported in the last block, but the `best_transactions` data has not been updated yet. Also investigate if Nonce Gaps in normal transactions or service transactions are leading to proposals. is `best_transactions` already a collection that is filtered for nonce gaps ?!

--- crates/ethcore/src/engines/hbbft/hbbft_engine.rs
+++ crates/ethcore/src/engines/hbbft/hbbft_engine.rs
@@ -1082,8 +1082,23 @@ impl HoneyBadgerBFT {
         if let Some(block_header) = client.block_header(BlockId::Latest) {
             let target_min_timestamp = block_header.timestamp() + self.params.minimum_block_time;
             let now = unix_now_secs();
+
+            // todo:
+            // empty blocks
+            // there could be cases, where the transactions that got included in the last block,
+            // have not been removed from the mem pool yet.
+            // we could check here the completion of the transaction pool maintenance,
+            // and either wait, or just do nothing in this case, since the next engine tick,
+            // will just call this again.
+
             // we could implement a cheaper way to get the number of queued transaction, that does not require this intensive locking.
             // see: https://github.com/DMDcoin/diamond-node/issues/237
+
+            // what is the best set of transactions ??
+            // pending_transactions() vs queued_transactions() vs ready_transactions()
+            // the proposal generation is done with "queued_transactions()" 
+            // queued transactions = transaction_queue.pending
+
             let queue_length = client.queued_transactions().len();
             (self.params.minimum_block_time == 0 || target_min_timestamp <= now)
                 && queue_length >= self.params.transaction_queue_size_trigger
